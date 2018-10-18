1b782720db2b71cf2ac5f95d69963b8df2306395
singhtej <singh.tej@hotmail.com>
2018-10-18 23:50:09 +0200

    add some code for triangle

--- DrawSquare.py
+++ DrawSquare.py
@@ -18,11 +18,21 @@ def draw_square():
     brad.right(90)
     brad.forward(100)
     brad.right(90)
-# Making a circle
+    # Draw a circle
     bingo = turtle.Turtle()
     bingo.shape("arrow")
     bingo.color("blue")
     bingo.circle(100)
+   # draw a tringle
+    tango = turtle.Turtle()
+    tango.shape("turtle")
+    tango.color("black")
+    tango.forward(100)
+    tango.left(120)
+    tango.forward(100)
+    tango.left(120)
+    tango.forward(100)
+
 
     window.exitonclick()
 draw_square()
