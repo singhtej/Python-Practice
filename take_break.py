e9100d802b43ccb02d54df770fc495de41aa8b86
singhtej <singh.tej@hotmail.com>
2018-10-18 15:11:36 +0200

    add function current time

--- take_break.py
+++ take_break.py
@@ -9,7 +9,8 @@ import time
 
 totak_break = 3
 break_count = 0
+print("Current Time "+time.ctime())
 while(break_count < totak_break):
-    time.sleep(7200) # seconds
+    time.sleep(3) # seconds
     webbrowser.open("https://www.youtube.com/watch?v=L1KHzzDs9vk&t=1006s")
     break_count = break_count + 1
