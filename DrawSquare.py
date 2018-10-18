58b34767ea060ec67c58ca614863950bccbdb6ec
singhtej <singh.tej@hotmail.com>
2018-10-18 23:31:45 +0200

    adding circle

--- DrawSquare.py
+++ DrawSquare.py
@@ -1,14 +1,15 @@
 import turtle
 
 def draw_square():
+   # window screen
     window = turtle.Screen()
     window.bgcolor("red")
-
+    #turtle class and methods
     brad = turtle.Turtle()
     brad.shape("turtle")
     brad.color("Green")
     brad.speed(2)
-
+    #size of the circle and instructiona
     brad.forward(100)
     brad.right(90)
     brad.forward(100)
@@ -17,6 +18,11 @@ def draw_square():
     brad.right(90)
     brad.forward(100)
     brad.right(90)
+# Making a circle
+    bingo = turtle.Turtle()
+    bingo.shape("arrow")
+    bingo.color("blue")
+    bingo.circle(100)
 
     window.exitonclick()
 draw_square()
